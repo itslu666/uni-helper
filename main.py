c63c1614ad2f4d49b5f78171d27a0bc621f75020
itslu666 <philipp.j6y@gmail.com>
2024-10-19 17:21:30 +0200

    add nav bar frame

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ root = ctk.CTk()
 root.title("Uni Helper")
 root.geometry("800x500")
 
+# configure grid layout for resizing
+root.grid_rowconfigure(0, weight=1)  # make row 0 expandable
+root.grid_columnconfigure(1, weight=1)  # make column 0 expandable
+
 # make widgets
+# frames
+nav_frame = ctk.CTkFrame(root)
+
 # tabview
 tabview = ctk.CTkTabview(root)
 tabview.add("Summary")
@@ -13,6 +20,7 @@ tabview.add("Quiz")
 tabview.add("Flash Cards")
 
 # show widgets
-tabview.pack(expand=True, fill="both", padx=10, pady=10)
+nav_frame.grid(row=0, column=0, sticky="nsew", pady=10, padx=(10, 5))
+tabview.grid(row=0, column=1, sticky="nsew", pady=10, padx=(5, 10))
 
 root.mainloop()
