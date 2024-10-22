829901f6822014917e13cd9bfff74c8aba237b23
leon-ma08 <leo.ma2608@gmail.com>
2024-10-22 12:53:35 +0200

    test

--- modules/new_window.py
+++ modules/new_window.py
@@ -1,24 +1,27 @@
 import customtkinter as ctk
-from tkinter import PhotoImage
+from PIL import Image
 def open_new():
     root = ctk.CTk()
 
     #window scaling
     root.geometry("700x400")
     root.title("New Lecture")
-    #rec button
-    img_rec_button = PhotoImage(file="data/images/rec-button.png")
-    button = ctk.CTkButton(root, command=button_click,
-                            border_color="red",
+    #load and rezize image
+    img_rec_button = Image.open("./data/images/rec-button.png")
+    #convert image in ctk compatible 
+    #tk_image = ImageTk.PhotoImage(img_rec_button)
+    button_image = ctk.CTkImage(img_rec_button)
+    #button 
+    button = ctk.CTkButton(root,
                             width=30,
                             height=30,
                             text="rec",
-                            image=img_rec_button)
-    button.grid(row=0, column=0, padx=20, pady=20)
+                            image=button_image,
+                            command=button_click)
     #drag and drop
 
 
-
+    button.pack()
     root.mainloop()
 #click for the rec button
 def button_click():
