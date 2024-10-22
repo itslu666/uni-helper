19bcc1e3bc598e28ff2cafbbf2740161d7686ebe
itslu666 <philipp.j6y@gmail.com>
2024-10-22 13:06:10 +0200

    fixed all the shit that happened

--- modules/new_window.py
+++ modules/new_window.py
@@ -1,35 +1,36 @@
 import customtkinter as ctk
 from PIL import Image
-def open_new():
-    root = ctk.CTk()
 
-    #window scaling
+
+def open_new():
+    root = ctk.CTkToplevel()
+
+    # window scaling
     root.geometry("700x400")
     root.title("New Lecture")
-    #load and rezize image
-    img_rec_button = Image.open("./data/images/rec-button.png")
-    #convert image in ctk compatible 
-    #tk_image = ImageTk.PhotoImage(img_rec_button)
-    button_image = ctk.CTkImage(img_rec_button)
-    #button 
-    button = ctk.CTkButton(root,
-                            width=30,
-                            height=30,
-                            text="rec",
-                            image=button_image,
-                            command=button_click)
-    #drag and drop
 
+    # button
+    button = ctk.CTkButton(
+        root,
+        width=30,
+        height=30,
+        text="",
+        image=ctk.CTkImage(Image.open("./data/images/rec-button.png"), size=(50, 50)),
+        command=button_click,
+    )
+    # drag and drop
 
     button.pack()
     root.mainloop()
-#click for the rec button
+
+
+# click for the rec button
 def button_click():
     print("buttonclick")
     return saund()
-    #nimmt dann auf
+    # nimmt dann auf
 
 
 def saund():
-    #recording of and aduio
+    # recording of and aduio
     print("klappt noch nicht")
